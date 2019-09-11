9a8691b07a7e50e15c447fcc90419422fb53f302
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-09-11 01:09:08 +0100

    improve merge method

--- merge_sort.rb
+++ merge_sort.rb
@@ -5,22 +5,18 @@ end
 
 def merge(arr1, arr2)
   temp = []
-  until arr1.empty? && arr2.empty?
-    if arr1[0].nil?
-      temp << arr2[0]
-      break
-    end
-    if arr2[0].nil?
-      temp << arr1[0]
-      break
-    end
-    if arr1[0] < arr2[0]
-        temp << arr1[0]
+  until arr1.empty? || arr2.empty?
+    if arr1.first < arr2.first
+        temp << arr1.first
         arr1.shift
     else
-      temp << arr2[0]
+      temp << arr2.first
       arr2.shift
     end
   end
-  temp
+  temp += (arr1 + arr2)
 end
+
+arr1 = [1,2,3]
+arr2 = [4,5,6]
+p merge arr1, arr2
